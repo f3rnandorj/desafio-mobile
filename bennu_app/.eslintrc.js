6e704f966119ef6c25aae5b9aee83a0c75cf9594
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2025-05-13 18:13:00 -0300

    Add husky pre commit. Remove unnecessary code.

--- bennu_app/.eslintrc.js
+++ bennu_app/.eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: "@react-native",
+  parserOptions: {
+    requireConfigFile: false,
+  },
+  rules: {
+    quotes: ["error", "doble"],
+  },
 };
