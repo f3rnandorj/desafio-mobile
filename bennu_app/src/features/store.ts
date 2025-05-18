449412ac7c9bfc5b972fa0b40f943309e24679dc
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2025-05-18 14:32:51 -0300

    Config wrappers to use redux. Add husky.

--- bennu_app/src/features/store.ts
+++ bennu_app/src/features/store.ts
@@ -1,30 +1,37 @@
-import {configureStore, combineReducers} from "@reduxjs/toolkit";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import {persistReducer, persistStore} from "redux-persist";
 
 import {MMKVStorage} from "@services";
 
 import todoReducer from "./todo/todoSlice";
 
+const rootReducer = combineReducers({
+  todo: todoReducer,
+});
+
 const persistConfig = {
   key: "root",
   storage: MMKVStorage,
 };
 
-const rootReducer = combineReducers({
-  todo: todoReducer,
-});
-
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
-});
+export type PersistedRootState = ReturnType<typeof persistedReducer>;
 
+export function setupStore(preloadedState?: PersistedRootState) {
+  return configureStore({
+    reducer: persistedReducer,
+    preloadedState,
+    middleware: getDefaultMiddleware =>
+      getDefaultMiddleware({
+        serializableCheck: false,
+      }),
+  });
+}
+
+export const store = setupStore();
 export const persistor = persistStore(store);
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore["dispatch"];
--- bennu_app/src/test/test-utils.tsx
+++ bennu_app/src/test/test-utils.tsx
@@ -7,10 +7,21 @@ import {
   renderHook,
   RenderHookOptions,
 } from "@testing-library/react-native";
+import {Provider} from "react-redux";
+import {persistStore} from "redux-persist";
+import {PersistPartial} from "redux-persist/es/persistReducer";
+import {PersistGate} from "redux-persist/integration/react";
 import {ThemeProvider} from "styled-components/native";
 
 import {ContextProviders, GlobalServiceComponents} from "@context";
+import {AppStore, PersistedRootState, RootState, setupStore} from "@features";
 import {theme} from "@theme";
+
+interface ExtendedRenderOptions
+  extends Omit<RenderOptions, "queries" | "wrapper"> {
+  preloadedState?: Partial<RootState> & PersistPartial;
+  store?: AppStore;
+}
 
 const wrapAllProviders = () => {
   return ({children}: {children: React.ReactNode}) => (
@@ -22,28 +33,46 @@ const wrapAllProviders = () => {
 
 function customRender<T = unknown>(
   component: ReactElement<T>,
-  options?: Omit<RenderOptions, "wrapper">,
+  options?: ExtendedRenderOptions,
 ) {
   return render(component, {wrapper: wrapAllProviders(), ...options});
 }
 
-const wrapScreenProviders = () => {
+//--------------------------------------------------------------------------//
+
+const wrapScreenProviders = (
+  extendedRenderOptions: ExtendedRenderOptions = {},
+) => {
+  const {
+    preloadedState = {_persist: {version: 1, rehydrated: true}},
+    // Automatically create a store instance if no store was passed in
+    store = setupStore(preloadedState as PersistedRootState),
+  } = extendedRenderOptions;
+
+  const persistor = persistStore(store);
+
   return ({children}: {children: React.ReactNode}) => (
     <ThemeProvider theme={theme}>
-      <ContextProviders>
-        <NavigationContainer>{children}</NavigationContainer>
-        <GlobalServiceComponents />
-      </ContextProviders>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ContextProviders>
+            <NavigationContainer>{children}</NavigationContainer>
+            <GlobalServiceComponents />
+          </ContextProviders>
+        </PersistGate>
+      </Provider>
     </ThemeProvider>
   );
 };
 
 export function renderScreen<T = unknown>(
   component: ReactElement<T>,
-  options?: Omit<RenderOptions, "wrapper">,
+  options: ExtendedRenderOptions = {},
 ) {
-  return render(component, {wrapper: wrapScreenProviders(), ...options});
+  return render(component, {wrapper: wrapScreenProviders(options), ...options});
 }
+
+//--------------------------------------------------------------------------//
 
 function customRenderHook<Result, Props>(
   renderCallback: (props: Props) => Result,
